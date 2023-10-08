c59f8943bcff15ce7c1b01722cf384f3f1470eab
Bartosz Kubek <kubek.bartosz@gmail.com>
2023-10-08 13:41:21 +0000

    - some fixes + new local provider used. But no resources yet

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-module "terrahouse-aws" {
-  source = "./modules/terrahouse-aws"
-  USER_UUID = var.USER_UUID
-  BUCKET_NAME = var.BUCKET_NAME
-  AWS_DEFAULT_REGION = var.AWS_DEFAULT_REGION
-  AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
-  AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
-  FILE_index_html = var.FILE_index_html
-  FILE_error_html = var.FILE_error_html
-  content_version = var.content_version
-  assets_path = var.assets_path
-}
+# module "terrahouse-aws" {
+#   source = "./modules/terrahouse-aws"
+#   USER_UUID = var.USER_UUID
+#   BUCKET_NAME = var.BUCKET_NAME
+#   AWS_DEFAULT_REGION = var.AWS_DEFAULT_REGION
+#   AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+#   AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
+#   FILE_index_html = var.FILE_index_html
+#   FILE_error_html = var.FILE_error_html
+#   content_version = var.content_version
+#   assets_path = var.assets_path
+# }
 
-module "random" {
-  source = "./modules/random"
-}
+# module "random" {
+#   source = "./modules/random"
+# }
 
--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-output "random_bucket_name" {
-  value = module.random.random_bucket_name
-}
-output "bucket_name" {
-  value = module.terrahouse-aws.bucket_name
-}
-output "website_endpoint" {
-  value = module.terrahouse-aws.website_endpoint
-}
-output "cloudfront_url" {
-  description = "The CloudFront Distribution Domain Name"
-  value = module.terrahouse-aws.cloudfront_url
-}
+# output "random_bucket_name" {
+#   value = module.random.random_bucket_name
+# }
+# output "bucket_name" {
+#   value = module.terrahouse-aws.bucket_name
+# }
+# output "website_endpoint" {
+#   value = module.terrahouse-aws.website_endpoint
+# }
+# output "cloudfront_url" {
+#   description = "The CloudFront Distribution Domain Name"
+#   value = module.terrahouse-aws.cloudfront_url
+# }
